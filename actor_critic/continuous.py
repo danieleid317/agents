b63d4df655ee322d4712d26730790ef4d25a4b40
danieleid317 <47306015+danieleid317@users.noreply.github.com>
2021-12-27 19:36:06 -0800

    continuous rnn agent

--- actor_critic/continuous.py
+++ actor_critic/continuous.py
@@ -1,9 +1,14 @@
+'''
+RNN Continuous Implementation
+'''
+
+
 import torch as T
 import torch.nn as nn
 import torch.nn.functional as F
 import torch.optim as optim
 from envs import PyBulletCartPoleEnv
-
+import statistics
 env = PyBulletCartPoleEnv()
 
 class RNN(nn.Module):
@@ -34,61 +39,68 @@ class Agent(object):
     self.gamma = gamma
     self.log_probs = []
     self.advantages = []
-    self.obs_traj = []
-
+    self.estimate = None
+    self.ground_truth = None
 
     self.actor = RNN( input_size , hidden_size=100 , output_size = n_outputs , lr = actor_lr )
     self.critic = RNN(input_size , hidden_size=100 , output_size=1 , lr = critic_lr)
-    self.actor_hidden = self.actor.init_hidden()
-    self.critic_hidden = self.critic.init_hidden()
+    self.actor_hidden = None
+    self.critic_hidden = None
 
-  def choose_action( self , observation ):
-    self.obs_traj.append( observation )
-    mu_sigma , hidden = self.actor.forward( self.obs_traj , self.actor_hidden )
-    self.actor_hidden = hidden
+  def choose_action( self , obs_traj ):
+    agent.actor_hidden = agent.actor.init_hidden()
+    x = T.unsqueeze(T.tensor(obs_traj),1)
+    for i in range(x.size()[0]):
+        mu_sigma , self.actor_hidden = self.actor.forward(x[i], self.actor_hidden)
     sigma = T.abs( mu_sigma[0][1] )
-    distribution = T.distributions.Normal ( mu_sigma[0][0] , mu_sigma[0][1] )
+    mu = T.tanh(mu_sigma[0][0])
+    distribution = T.distributions.Normal ( mu , sigma )
     torque = distribution.sample(sample_shape = (1,))
     self.log_probs = distribution.log_prob(torque)
     action = T.tanh(torque)
     return action.item()
 
-  def log_step(self , reward , new_state , done):
-    critic_value , hidden = self.critic.forward(self.obs_traj , self.critic_hidden)
-    self.critic_hidden = hidden
-    #self.estimates.append( critic_value )
+  def log_step(self , reward , obs_traj_ , prime_state , done):
+    x = T.unsqueeze(T.tensor(obs_traj_),1)
+    agent.critic_hidden = agent.critic.init_hidden()
+    for i in range(x.size()[0]):
+        critic_value, self.critic_hidden = self.critic.forward(x[i], self.critic_hidden)
+
+    self.estimate = critic_value
     reward = T.tensor( reward , dtype = T.float )
-    self.obs_traj.append( new_state )
+
+    obs_traj_.append(prime_state)
+    next_state_traj = T.unsqueeze(T.tensor(obs_traj_),1)
+    agent.critic_hidden = agent.critic.init_hidden()
     with T.no_grad():
-      critic_value_ , _ = self.critic.forward(self.obs_traj , self.critic_hidden)
-    self.obs_traj.drop( new_state )
-    #self.ground_truths = reward + critic_value_
-    advantage = reward + self.gamma*critic_value_*(1-int(done)) - critic_value
+      for i in range(next_state_traj.size()[0]):
+        critic_value_ , self.critic_hidden = self.critic.forward( next_state_traj[i] , self.critic_hidden)
+    self.ground_truth = reward + critic_value_
+    advantage = reward  + self.gamma*critic_value_*(1-int(done)) - critic_value
     self.advantages =  advantage
 
   def learn( self , ):
     actor_loss = -self.log_probs * self.advantages
-    critic_loss = T.reduce_difference( self.estimates - self.ground_truths ) ** 2
+    critic_loss = ( self.estimate - self.ground_truth ) ** 2
     ( actor_loss + critic_loss ).backward()
     self.actor.optimizer.step()
     self.critic.optimizer.step()
     self.actor.optimizer.zero_grad()
     self.critic.optimizer.zero_grad()
 
-agent = Agent( actor_lr = 0.001 , critic_lr = .001 , input_dims = 4 , gamma = .9 , layer1_size = 256 , layer2_size = 256 , n_outputs = 2 )
+agent = Agent( actor_lr = 0.000001 , critic_lr = .000001 , input_dims = 4 , gamma = .9 , layer1_size = 256 , layer2_size = 256 , n_outputs = 2 )
 
-num_episodes = 50000
+lens = []
+num_episodes = 50
 for i in range(num_episodes):
-
+  obs_traj = []
   done = False
-  score = 0
   env.reset()
-  observation = env.state
   while not done :
-    action = agent.choose_action(env.state)
+    obs_traj.append( env.state.tolist() )
+    action = agent.choose_action(obs_traj)
     reward , done = env.step(action)
-    observation_ = env.state
-    agent.log_step( reward , observation_ , done)
-    score += reward
+    agent.log_step( reward , obs_traj , env.state.tolist() , done)
     agent.learn()
-  print(score)
+  lens.append(len(obs_traj))
+  print(statistics.mean(lens[-10:]))
